413bfc7e5086d1c03e9a9d5160652202fd4aeb48
Alessandro Buson <buson.alessandro@gmail.com>
2016-09-29 15:32:16 +0200

    First import

--- src/main/java/it/mytest/salestaxes/App.java
+++ src/main/java/it/mytest/salestaxes/App.java
@@ -1,8 +1,8 @@
 package it.mytest.salestaxes;
 
-import it.mytest.salestaxes.product.BookProduct;
-import it.mytest.salestaxes.product.DefaultProduct;
-import it.mytest.salestaxes.product.FoodProduct;
+import it.mytest.salestaxes.product.BookProductFactory;
+import it.mytest.salestaxes.product.DefaultProductFactory;
+import it.mytest.salestaxes.product.FoodProductFactory;
 import it.mytest.salestaxes.store.Counter;
 import it.mytest.salestaxes.store.Receipt;
 import it.mytest.salestaxes.store.ShoppingCart;
@@ -17,9 +17,13 @@ public class App
     public static void main( String[] args )
     {
     	ShoppingCart shoppingCart = new ShoppingCart();
-    	shoppingCart.addProductToCart(new BookProduct("book", 12.49, false, 1));
-    	shoppingCart.addProductToCart(new DefaultProduct("music CD", 14.99, false, 1));
-    	shoppingCart.addProductToCart(new FoodProduct("chocolate bar", 0.85, false, 1));
+    	BookProductFactory bookProductFactory = new BookProductFactory();
+    	DefaultProductFactory defaultProductFactory = new DefaultProductFactory();
+    	FoodProductFactory foodProductFactory = new FoodProductFactory();
+    	
+    	shoppingCart.addProductToCart(bookProductFactory.getProduct("book", 12.49, false, 1));
+    	shoppingCart.addProductToCart(defaultProductFactory.getProduct("music CD", 14.99, false, 1));
+    	shoppingCart.addProductToCart(foodProductFactory.getProduct("chocolate bar", 0.85, false, 1));
     	
     	Counter counter = new Counter(shoppingCart);
     	counter.setTaxCalculator(new TaxCalculator());
--- src/test/java/it/mytest/salestaxes/AppTest.java
+++ src/test/java/it/mytest/salestaxes/AppTest.java
@@ -1,9 +1,9 @@
 package it.mytest.salestaxes;
 
-import it.mytest.salestaxes.product.BookProduct;
-import it.mytest.salestaxes.product.DefaultProduct;
-import it.mytest.salestaxes.product.FoodProduct;
-import it.mytest.salestaxes.product.MedicalProduct;
+import it.mytest.salestaxes.product.BookProductFactory;
+import it.mytest.salestaxes.product.DefaultProductFactory;
+import it.mytest.salestaxes.product.FoodProductFactory;
+import it.mytest.salestaxes.product.MedicalProductFactory;
 import it.mytest.salestaxes.store.Counter;
 import it.mytest.salestaxes.store.Receipt;
 import it.mytest.salestaxes.store.ShoppingCart;
@@ -18,6 +18,23 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
+	
+	/**
+	 * Factory book product
+	 */
+	BookProductFactory bookProductFactory = new BookProductFactory();
+	/**
+	 * Factory default product
+	 */
+	DefaultProductFactory defaultProductFactory = new DefaultProductFactory();
+	/**
+	 * Factory food product
+	 */
+	FoodProductFactory foodProductFactory = new FoodProductFactory();
+	/**
+	 * Factory medical product
+	 */
+	MedicalProductFactory medicalProductFactory = new MedicalProductFactory();
     /**
      * Create the test case
      *
@@ -42,9 +59,9 @@ public class AppTest
     public void testInput1()
     {
     	ShoppingCart shoppingCart = new ShoppingCart();
-    	shoppingCart.addProductToCart(new BookProduct("book", 12.49, false, 1));
-    	shoppingCart.addProductToCart(new DefaultProduct("music CD", 14.99, false, 1));
-    	shoppingCart.addProductToCart(new FoodProduct("chocolate bar", 0.85, false, 1));
+    	shoppingCart.addProductToCart(bookProductFactory.getProduct("book", 12.49, false, 1));
+    	shoppingCart.addProductToCart(defaultProductFactory.getProduct("music CD", 14.99, false, 1));
+    	shoppingCart.addProductToCart(foodProductFactory.getProduct("chocolate bar", 0.85, false, 1));
     	
     	Counter counter = new Counter(shoppingCart);
     	counter.setTaxCalculator(new TaxCalculator());
@@ -58,8 +75,8 @@ public class AppTest
     public void testInput2()
     {
     	ShoppingCart shoppingCart = new ShoppingCart();
-    	shoppingCart.addProductToCart(new FoodProduct("box of chocolates", 10.00, true, 1));
-    	shoppingCart.addProductToCart(new DefaultProduct("bottle of perfume", 47.50, true, 1));
+    	shoppingCart.addProductToCart(foodProductFactory.getProduct("box of chocolates", 10.00, true, 1));
+    	shoppingCart.addProductToCart(defaultProductFactory.getProduct("bottle of perfume", 47.50, true, 1));
     	
     	Counter counter = new Counter(shoppingCart);
     	counter.setTaxCalculator(new TaxCalculator());
@@ -73,10 +90,10 @@ public class AppTest
     public void testInput3()
     {
     	ShoppingCart shoppingCart = new ShoppingCart();
-    	shoppingCart.addProductToCart(new DefaultProduct("bottle of perfume", 27.99, true, 1));
-    	shoppingCart.addProductToCart(new DefaultProduct("bottle of perfume", 18.99, false, 1));
-    	shoppingCart.addProductToCart(new MedicalProduct("packet of headache", 9.75, false, 1));
-    	shoppingCart.addProductToCart(new FoodProduct("chocolates", 11.25, true, 1));
+    	shoppingCart.addProductToCart(defaultProductFactory.getProduct("bottle of perfume", 27.99, true, 1));
+    	shoppingCart.addProductToCart(defaultProductFactory.getProduct("bottle of perfume", 18.99, false, 1));
+    	shoppingCart.addProductToCart(medicalProductFactory.getProduct("packet of headache", 9.75, false, 1));
+    	shoppingCart.addProductToCart(foodProductFactory.getProduct("chocolates", 11.25, true, 1));
     	
     	Counter counter = new Counter(shoppingCart);
     	counter.setTaxCalculator(new TaxCalculator());
